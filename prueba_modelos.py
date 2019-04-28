da9bbcb2182a59668c7be8fd787990663f089417
Martín <marsancoo@gmail.com>
2019-04-28 18:30:31 -0300

    Agrego funcionalidad para probar diferentes periodos granuralidad y lags

--- prueba_modelos.py
+++ prueba_modelos.py
@@ -1,6 +1,5 @@
 import numpy as np
 np.random.seed(1337) # for reproducibility
-from sklearn.model_selection import train_test_split
 from keras.layers import Conv1D, LSTM, Dropout, Dense, Flatten, BatchNormalization, Activation, Input, AveragePooling1D, GlobalAveragePooling1D
 from sklearn.preprocessing import StandardScaler, FunctionTransformer
 from tcn import compiled_tcn
@@ -59,7 +58,7 @@ def get_architecture(n):
                              dilations=[1, 2, 4],
                              # dilations=[2 ** i for i in range(2, 5)],
                              nb_stacks=1,
-                             max_len=time_lags[n],
+                             max_len=time_lags,
                              use_skip_connections=True,
                              regression=True,
                              dropout_rate=0.4)
@@ -99,7 +98,8 @@ numeric_cols = ['stationaryLevel', 'walkingLevel', 'runningLevel',
 
 df = pd.read_pickle('pkl/dataset.pkl')
 time_lags = 4
-epochs=256
+period = 24
+epochs=4
 batch=64
 user = 57
 metric = 'mae'
@@ -108,17 +108,29 @@ impersonal = True
 
 
 for col in numeric_cols:
-    for lag in range(1, time_lags + 1):
+    for lag in range(0, time_lags):
         to_standarize.append(col + '(t-{0})'.format(lag))
 
 def get_model(input_shape):
+    model = compiled_tcn(return_sequences=False,
+                         num_feat=number_of_features,
+                         num_classes=0,
+                         nb_filters=64,
+                         kernel_size=2,
+                         dilations=[1, 2],
+                         # dilations=[2 ** i for i in range(2, 5)],
+                         nb_stacks=4,
+                         max_len=time_lags,
+                         use_skip_connections=True,
+                         regression=True,
+                         dropout_rate=0)
+    '''
     model = Sequential()
-    model.add(LSTM(4, input_shape=input_shape, return_sequences=False))
-    model.add(Dropout(.8))
-    model.add(Dense(1, activation='linear'))
+    model.add(Dense(1, input_dim=input_shape, activation='linear'))
     model.compile(loss='mae',
                   optimizer='adam',
                   )
+    '''
     print(model.summary())
     return model
 
@@ -169,21 +181,24 @@ tcn
 
 
 if impersonal:
-    data = pd.read_pickle('pkl/dataset_lags{0}.pkl'.format(time_lags)), user
+    data = pd.read_pickle('pkl/datasets/dataset_lags{0}_period{1}_gran1h.pkl'.format(time_lags, period))
 else:
     data = get_user_data(pd.read_pickle('pkl/dataset_lags{0}.pkl'.format(time_lags)), user)
 
 number_of_features = df.shape[1]
-input_shape = (time_lags, number_of_features)
-x = data.iloc[:, 0:time_lags * number_of_features]
+input_shape = (time_lags,number_of_features)
+
+x = data.iloc[:, 0:-1]
 y = data.iloc[:, -1]
+
 if impersonal:
-    x_test = get_user_data(user)
-    x_train =
-    y_test = get_user_data(user)
-    y_train =
+    x_test = get_user_data(x, user)
+    x_train = get_not_user_data(x, user)
+    y_test = get_user_data(y, user)
+    y_train = get_not_user_data(y, user)
 else:
     x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=False, train_size=0.67)
+
 x_train.loc[:, to_standarize] = ss.fit_transform(x_train[to_standarize])
 x_test.loc[:, to_standarize] = ss.transform(x_test[to_standarize])
 x_train, y_train, x_test, y_test = x_train.values.astype("float32"), y_train.values.astype("float32"), \
@@ -192,8 +207,8 @@ x_train, y_train, x_test, y_test = x_train.values.astype("float32"), y_train.val
 #estandarizar salida
 
 if tresd:
-    x_train = x_train.reshape(x_train.shape[0], time_lags, number_of_features)
-    x_test = x_test.reshape(x_test.shape[0], time_lags, number_of_features)
+    x_train = x_train.reshape(x_train.shape[0], time_lags,number_of_features)
+    x_test = x_test.reshape(x_test.shape[0], time_lags,number_of_features)
 else: input_shape = data.shape[1]-1
 
 print('{0} casos de entrenamiento. **** {1} casos para testeo.'.format(x_train.shape[0], x_test.shape[0]))
@@ -201,7 +216,7 @@ model = get_model(input_shape)
 history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch,
                     validation_data=(x_test, y_test),
                     shuffle=False,
-                    verbose=0)
+                    verbose=2)
 
 plt.close()
 plt.figure(figsize=(15, 4))
