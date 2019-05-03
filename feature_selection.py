16edeead404d2c50719ac16820b9d2a8681c779c
Martín <marsancoo@gmail.com>
2019-05-03 12:48:36 -0300

    Agrego scrip que muestra la importancia de cada feature

---
+++ feature_selection.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2, f_regression
+from utils import get_data,split_x_y
+
+data = get_data(False,3,1,'1h')
+X,y = split_x_y(data)
+
+
+bestfeatures = SelectKBest(score_func=f_regression, k=10)
+fit = bestfeatures.fit(X,y)
+dfscores = pd.DataFrame(fit.scores_)
+dfcolumns = pd.DataFrame(X.columns)
+#concat two dataframes for better visualization
+featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+featureScores.sort_values('Score', inplace=True)
+print(featureScores.nlargest(24,'Score'))  #print 10 best features
+
+
+
+from sklearn.ensemble import ExtraTreesRegressor
+import matplotlib.pyplot as plt
+model = ExtraTreesRegressor()
+model.fit(X,y)
+print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
+#plot graph of feature importances for better visualization
+feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+feat_importances.nlargest(10).plot(kind='barh')
+plt.show()
