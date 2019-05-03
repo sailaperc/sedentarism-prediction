45aef683314fe330f7751e7970eb6c19afaf0d74
Martín <marsancoo@gmail.com>
2019-05-03 12:47:22 -0300

    Agrego funcionalidad para mostrar los baseline models

--- baseline_models.py
+++ baseline_models.py
@@ -2,15 +2,21 @@ from os import listdir
 from os.path import isfile, join
 from sklearn.linear_model import LinearRegression
 import pandas as pd
-from utils import get_data, split_x_y
+from utils import get_data, get_train_test_data
 from sklearn.metrics import mean_absolute_error
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 path = 'pkl/datasets'
 files = listdir(path)
-scores = list()
+
+df = pd.DataFrame(columns=['gran','periods','lags','mae'])
+
 for f in files:#files:
+    print(f)
     i_lags = f.find('lags')
     i_period = f.find('period')
     i_gran = f.find('gran')
@@ -25,14 +31,21 @@ for f in files:#files:
     print('lags :' + str(lags) + '. period: ' + str(period) + '. gran: ' + str(gran))
 
     data = get_data(False, lags, period, gran, user)
-    x_train, y_train, x_test, y_test = split_x_y(data,False, lags, gran, False, user)
+    x_train, y_train, x_test, y_test = get_train_test_data(user, True, lags, period, gran,True)
     lr = LinearRegression()
     lr.fit(x_train,y_train)
     s = 'lags :' + str(lags) + '. period: ' + str(period) +\
         '. gran: ' + str(gran) + '. score: ' + str(lr.score(x_test,y_test))
-    scores.append('lags :' + str(lags) + '. period: ' + str(period) + '. gran: ' + str(gran) +
-                  '. mae: ' + str(round(mean_absolute_error(y_test, lr.predict(x_test)),3)))
+    new = pd.DataFrame(
+        {'lags' : [lags],
+         'period' : period,
+         'gran' : gran,
+         'mae' : float(round(mean_absolute_error(y_test, lr.predict(x_test)), 3))})
+    df = df.append(new)
 
 
-for score in scores:
-    print(score)
+
+g = sns.FacetGrid(df, col='gran')
+g.map(plt.scatter,'period','mae')
+plt.show()
+
