99c8ee37b65214cb1f0fc0f6375dd14b03685bec
Martín Santillán Cooper <marsancoo@gmail.com>
2020-06-25 14:58:04 +0200

    Agrego funciones parageneracion de graficos y analisis de sensores

--- data_analysis/inference.py
+++ data_analysis/inference.py
@@ -4,19 +4,44 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from preprocessing.studentlife_raw import get_sensor_data
-sns.set(color_codes=True)
-sns.set_style("whitegrid", {'axes.grid' : False})
-sensor_data_files = ['activity', 'audio', 'gps', 'dark',
-                         'phonelock', 'wifi', 'phonecharge',
-                         'calendar', 'wifi_location', 'conversation']
+sns.set_style("whitegrid")
 
-def plot_inference_info():
+def plot_unknown_per_user():
     df = get_sensor_data('activity')
     df.columns = ['time', 'inference', 'userId']
-    df = df.loc[df['inference'] == 3]
-    min_date = datetime.fromtimestamp(min(df.time))
+    df['time'] = pd.to_datetime(df['time'], unit='s').dt.floor('1h')
+
+    df_all = df.groupby(['userId','time'], as_index=False).size().to_frame('count')
+    df_all.reset_index(inplace=True)
+
+    df = df[df['inference'] == 3]
+    df_unknowns = df[df['inference'] == 3].groupby(['userId','time'], as_index=False).size().to_frame('count')
+    df_unknowns.reset_index(inplace=True)
+
+    plt.close()
+    fig, (ax,ax2) = plt.subplots(2, 1, figsize=(8, 6), sharex='all')
+    sns.barplot(x="userId", y="count", data=df_unknowns, ax=ax,
+                palette=sns.color_palette("Paired", 10))
+    plt.xticks(rotation='vertical')
+    ax.set_xlabel("")
+    ax.set_ylabel("Registros desconocidos")
+    ax.set_title('Registros desconocidos por usuario')
+
+    sns.barplot(x="userId", y="count", data=df_all, ax=ax2,
+                palette=sns.color_palette("Paired", 10))
+    ax2.set_ylabel("Registros totales")
+    ax2.set_xlabel("User ID")
+    plt.show()
+
+plot_unknown_per_user()
+
+def plot_activity_unknown_cumsum():
+    df = get_sensor_data('activity')
+    df.columns = ['time', 'inference', 'userId']
+    df = df[df['inference'] == 3]
+    min_date = datetime.fromtimestamp(df.time.min())
     min_date = min_date.replace(minute=0, second=0, microsecond=0)
-    max_date = datetime.fromtimestamp(max(df.time))
+    max_date = datetime.fromtimestamp(df.time.max())
     max_date = max_date.replace(minute=0, second=0, microsecond=0)
     print(min_date)
     s = pd.DataFrame(index=pd.MultiIndex.from_product(iterables=[df['userId'].drop_duplicates(),
@@ -26,41 +51,39 @@ def plot_inference_info():
                                                       names=['userId', 'time']))
     print(s)
     df['time'] = pd.to_datetime(df['time'], unit='s').dt.floor('1h')
-    grouped_per_hour_and_user = df.groupby(['userId', 'time'])['inference'].count()
-    print(grouped_per_hour_and_user)
-    s.loc[:, 'inference'] = grouped_per_hour_and_user
+    df_size = df.groupby(['userId', 'time']).size()
+    print(df_size)
+    s.loc[:, 'inference'] = df_size
     s = s.fillna(0)
     vals = s.to_numpy()
-
-
+    vals = np.cumsum(vals)
 
     plt.close()
-    plt.title('Registros desconocidos')
     fig = plt.figure()
-    ax = fig.add_subplot(121)
-    ax.plot(vals)
-    ax.set_xlabel('Horas')
-    ax.set_ylabel('Numero de valores desconocidos')
+    # ax = fig.add_subplot(121)
+    # ax.plot(vals)
+    # ax.set_xlabel('Horas')
+    # ax.set_ylabel('Suma acumulativa de valores desconocidos')
 
-    ax2 = fig.add_subplot(122)
-    vals = np.cumsum(vals)
+    ax2 = fig.add_subplot(111)
     ax2.plot(vals)
-    ax2.set_xlabel('Horas')
-    ax2.set_ylabel('Numero de valores desconocidos')
-
+    ax2.set_xlabel('Buckets de una hora')
+    ax2.set_ylabel('Registros desconocidos')
+    ax2.set_title('Suma acumulativa de registros desconocidos por hora')
     plt.subplots_adjust()
-
 
     fig.show()
 
+plot_activity_unknown_cumsum()
+
 def print_basic_info(sensor):
     '''
-    plots info of activity data
+    plots info of sensor data such as :
+    - nb_reg
     :return:
     '''
     print(f'basic info for {sensor}')
     df = get_sensor_data(sensor)
-    df.columns = ['time', 'inference', 'userId']
     nb_regs = df.shape[0]
     print(f'Cantidad total de registros: {nb_regs}')
 
@@ -74,35 +97,88 @@ def print_basic_info(sensor):
     '''
     del df
 
-def print_frec_info(sensor, freq):
+def print_tsd_info(sensor, freq):
+    '''
+    print info related to the frequency of the sensor data
+
+    :param sensor:
+    :param freq:
+    :return:
+    '''
     print(f'freq info for {sensor} and {freq}')
     df = get_sensor_data(sensor)
-    df.columns = ['time', 'inference', 'userId']
-    df = df.loc[df['inference'] != 3]
-    min_date = datetime.fromtimestamp(min(df.time))
-    max_date = datetime.fromtimestamp(max(df.time))
+
+    if 'time' in df.columns:
+        time = 'time'
+    else: time = 'timestamp'
+
+    df = df.loc[df[' activity inference'] != 3]
+
+    min_date = datetime.fromtimestamp(min(df[time]))
+    max_date = datetime.fromtimestamp(max(df[time]))
     s = pd.DataFrame(index=pd.MultiIndex.from_product(iterables=[df['userId'].drop_duplicates(),
                                                                          pd.date_range(min_date,
                                                                                        max_date,
                                                                                        freq=freq)],
-                                                              names=['userId', 'time']))
-
+                                                              names=['userId', time]))
     print(f'Total number of 1h blocks : {s.shape[0]}')
 
-    df['time'] = pd.to_datetime(df['time'], unit='s').dt.floor(freq)
-    grouped_per_hour_and_user = df.groupby(['userId', 'time'])['inference'].count()
-    print(f'Total number of {freq} blocks with data available: {grouped_per_hour_and_user.shape[0]}')
+    df[time] = pd.to_datetime(df[time], unit='s').dt.floor(freq)
+    grouped_per_hour_and_user = df.groupby(['userId', time]).size()
+    print(f'Total number of {freq} blocks with data available: {len(grouped_per_hour_and_user)}')
     vals = grouped_per_hour_and_user.to_numpy()
     print(f'Average and std available logs for {freq}: {vals.mean()} / {vals.std()}')
+    print(f'Min and max available logs for {freq}: {vals.min()} / {vals.max()}')
+
     del vals
     del df
     del grouped_per_hour_and_user
 
+def print_tsi_info(sensor, freq):
+    '''
+    print info related to the frequency of the sensor data
+
+    :param sensor:
+    :param freq:
+    :return:
+    '''
+    print(f'freq info for {sensor} and {freq}')
+    df = get_sensor_data(sensor)
+
+    if 'start' in df.columns:
+        start = 'start'
+        end = 'end'
+    else:
+        start = 'start_timestamp'
+        end = ' end_timestamp'
+
+    df['diff'] = df[end] - df[start]
+
+    print(f'Promedio y desviación estándar del tamaño de los intervalos: '
+          f'{ round(df["diff"].mean() / 3600, 3) } / {round(df["diff"].std() / 3600, 3) }')
+    print(f'Maximo y minimo del tamaño de los intervalos: '
+          f'{round(df["diff"].min() / 3600, 3)} / {round(df["diff"].max() / 3600, 3)}')
+
+print_tsi_info('dark','1h')
+
 def print_all_info():
-    for t in ['activity', 'audio']:
+
+    TSD = ['activity', 'audio', 'gps', 'bt',
+           'wifi', 'calendar', 'wifi_location']
+    TSI = ['dark', 'phonelock',
+            'phonecharge', 'conversation']
+
+    # for t in TSD:
+    #     print_basic_info(t)
+    #     for f in ['1h','30min']:
+    #         print_tsd_info(t, f)
+
+    for t in TSI:
         print_basic_info(t)
         for f in ['1h','30min']:
-            print_frec_info(t, f)
+            print_tsi_info(t, f)
+
+print_all_info()
 
 '''
 
@@ -126,5 +202,6 @@ plt.show()
 '''
 
 
-plot_inference_info()
 
+df = get_sensor_data('activity')
+df.userId.mean()
