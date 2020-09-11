87cd5487bae80ce4669493d2e9f6bef82549ddfb
Martín Santillán Cooper <marsancoo@gmail.com>
2020-09-12 00:01:26 +0200

    modified git ignore to commit checkpoint_tunning files

--- tunning/tcn.py
+++ tunning/tcn.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 from experiments.Experiment import PersonalExperiment, ImpersonalExperiment
 import tensorflow as tf
 import numpy as np
@@ -22,7 +22,7 @@ from skopt import load
 
 from utils.utils import file_exists
 #from numpy.random import seed
-#seed(1)
+# seed(1)
 seed = 1
 tf.random.set_seed(seed)
 
@@ -42,12 +42,15 @@ dimensions = [dim_num_filters,
               dim_num_epochs,
               dim_batch_size]
 
+
 def create_model_fn(num_filters, kernel_size, dropout, use_skip_connections, use_batch_norm, nb_lags):
-    nb_dilations = max(math.ceil(math.log(nb_lags/kernel_size,kernel_size)+1),2) 
-    if use_skip_connections==1 and nb_dilations==1:
-        nb_dilations+=1
+    nb_dilations = max(
+        math.ceil(math.log(nb_lags/kernel_size, kernel_size)+1), 2)
+    if use_skip_connections == 1 and nb_dilations == 1:
+        nb_dilations += 1
     print('dilations:', nb_dilations)
     print('receptive field: ', (kernel_size**(nb_dilations-1))*kernel_size)
+
     def create_model():
         model = Sequential(name='tcn')
         dilations = [2 ** i for i in range(nb_dilations)]
@@ -66,10 +69,11 @@ def create_model_fn(num_filters, kernel_size, dropout, use_skip_connections, use
             use_layer_norm=False))
         model.add(Dense(1, activation='linear'))
         model.compile(loss='MSE',
-                      optimizer='adam',
+                      optimizer='adam', metrics=keras.metrics.MSE
                       )
         return model
     return create_model
+
 
 @use_named_args(dimensions=dimensions)
 def fitness(num_filters, kernel_size, dropout, use_skip_connections, use_batch_norm, num_epochs, batch_size):
@@ -84,7 +88,7 @@ def fitness(num_filters, kernel_size, dropout, use_skip_connections, use_batch_n
     print()
 
     model_fn = create_model_fn(
-        num_filters, kernel_size, dropout, use_skip_connections, use_batch_norm,4)
+        num_filters, kernel_size, dropout, use_skip_connections, use_batch_norm, 4)
 
     pe = PersonalExperiment(model_fn, 'tcn', 'regression', 32, 4, 1, 60, True)
     pe.run(2**num_epochs, 2**batch_size, verbose=1)
@@ -94,12 +98,12 @@ def fitness(num_filters, kernel_size, dropout, use_skip_connections, use_batch_n
     return score
 
 
-#%%
-checkpoint_file = 'pkl/checkpoint_tcn_32_per.pkl' 
+# %%
+checkpoint_file = 'pkl/checkpoint_tcn_32_per.pkl'
 checkpoint_saver = CheckpointSaver(checkpoint_file, compress=9)
 
 
-#%%
+# %%
 res = load(checkpoint_file)
 x0 = res.x_iters
 y0 = res.func_vals
@@ -107,9 +111,9 @@ print(len(x0))
 sorted(zip(y0, x0))
 
 
-#%%
+# %%
 if (file_exists(checkpoint_file)):
-    
+
     search_result = gp_minimize(func=fitness,
                                 dimensions=dimensions,
                                 x0=x0,
@@ -135,9 +139,8 @@ print(search_result.fun)
 
 sorted(zip(search_result.func_vals, search_result.x_iters))
 
- 
 
-#%%
+# %%
 # personal / tcn / 34
 # (0.2578, [4, 4, 0.8, 1, 1, 8, 3])
 
