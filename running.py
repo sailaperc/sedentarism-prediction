1b2f1a287d023302e60964bfbc18aa7ed1717c6a
Martín Santillán Cooper <marsancoo@gmail.com>
2020-09-23 06:46:18 +0200

    discard wrong experiments

--- running.py
+++ running.py
@@ -3,6 +3,7 @@ import os
 from experiments.experiment_running import run_all_experiments
 import pickle as pkl
 
+# %%
 
 
 def fitness():
@@ -12,13 +13,20 @@ def fitness():
 run_all_experiments(verbose=1)
 
 
-# %%
+# # %%
 # def f():
 #     path_to_file = './pkl/experiments'
+#     c = 0
 #     for fn in os.listdir(path_to_file):
 #         nfn = fn[:-4] + '_per' + fn[-4:]
 #         os.rename(f'{path_to_file}/{fn}', f'{path_to_file}/{nfn}')
 
 
-# f()
-# %%
+# # %%
+# path_to_file = './pkl/experiments'
+# c = 0
+# for fn in os.listdir(path_to_file):
+#     if str(fn[:-4][-3:]) != 'per':
+#         os.remove(f'{path_to_file}/{fn}')
+#         c += 1
+# print(c)
