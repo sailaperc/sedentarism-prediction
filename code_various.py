3f9379c5111ec89b41d8ef27aeea9846c1ee87bc
Martin Santillan Cooper <marsancoo@gmail.com>
2020-11-11 23:22:27 -0300

    Code for plotting results

--- code_various.py
+++ code_various.py
@@ -1,23 +1,34 @@
 #%% 
-from utils.utils_graphic import plot_heatmaps_mean, plot_heatmaps_std
+from experiments.experiments_results import get_experiments_data, generate_df_from_experiments
+import matplotlib.pyplot as plt 
+import numpy as np
+df = get_experiments_data(with_y_test_pred=True)
 
-plot_heatmaps_mean(users=[3, 46, -1])
-plot_heatmaps_std(users=[3, 46, -1])
+def get_test_predicted_arrays(exp_data):
+    zipped = zip(*exp_data)
+    l = list(zipped)
+    l[1] = [np.squeeze(a) for a in l[1]]
+    y_test = np.concatenate(l[0]) 
+    y_pred = np.concatenate(l[1]) 
+    return y_test, y_pred
 
-#%%
-from preprocessing.datasets import get_lagged_dataset
-get_lagged_dataset(nb_lags=2,period=1, nb_min=60).shape
-#%%
-a = list(range(10))
-a[0:len(a):3]
-(len(df.columns)-1)/4
+def print_results(fromi=1, toi=5, archs=['rnn', 'tcn', 'cnn', 'mlp'], poi='per', user=32, lags=1, period=1, gran=60):
+    exp = df.loc[((df.poi==poi) & (df.user==user) & (df.nb_lags==lags) & (df.period==period) & (df.gran==gran))]
+    plt.close()
+    width = 2 + 2*(toi-fromi+1)
+    plt.figure(figsize=(width,4))
+    print(width)
+    first_pass = True
+    for arch in archs:
+        exp_arch = exp.loc[df.arch==arch,:]
+        exp_data = exp_arch.y_test_pred.values[0][fromi-1:toi]
+        y_test, y_pred = get_test_predicted_arrays(exp_data)
+        lw = .6
+        if first_pass:
+            plt.plot(y_test, label='Test', lw=lw)
+            first_pass = False
+        plt.plot(y_pred, label=f'Predicho ({arch})', lw=lw)
+    plt.legend()
+    plt.show()
 
-#%%
-from preprocessing.datasets import get_clean_dataset
-df = get_clean_dataset(nb_min=30, dropna=True, from_disc=False)
-df.shape
-
-# %%
-from utils.utils_graphic import plot_user_selection
-plot_user_selection(2)
-    
+print_results(fromi=1, toi=5, loop_over={'period':[1,4],'archs':['tcn']}, poi='imp', lags=8, user=12)
