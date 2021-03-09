2413d03f3a13fb34d620ebfa37141dffa74c2629
Martin Santillan Cooper <marsancoo@gmail.com>
2021-03-08 22:34:35 -0300

    added function for getting tunning results

--- src/tunning/utils.py
+++ src/tunning/utils.py
@@ -1,29 +1,42 @@
-#%%
-import sys
-sys.path.insert(0, "c:\\Users\\marsa\\Documents\\projects\\tesis-project\\src")
-print(sys.path[0])
-
-#%%
+import numpy as np
 import pandas as pd
+
 from skopt import load
 
-#%%
-def fitness():
-    pass
-for poi in ['per','imp']:
-    for u in [32,34]:
-        for arch in ['cnn','tcn','rnn','mlp']:
-            filename = f'../../pkl/tunning/checkpoint_{arch}_{u}_{poi}.pkl'
-            res = load(filename)
-            x0 = res.x_iters
-            y0 = res.func_vals
-            print(len(x0))
-            sorted(zip(y0, x0))
 
-#%%
-res = load('../../pkl/tunning/checkpoint_cnn_32_imp.pkl')
-#%%
-x0 = res.x_iters
-y0 = res.func_vals
-print(len(x0))
-sorted(zip(y0, x0))
+
+def get_tunning_results(arch):
+   
+    cnn_hypers = ['Nr. filtros', 'Tam. kernel', 'Dropout (conv)', 'Nr. nodos densos', 'Dropout (denso)', 'Nr. épocas', 'Tam. bache', 'MSE']
+    mlp_hypers = ['Nr. nodos', 'Nr. capas', 'Usar Batch Normalization', 'Dropout', 'Nr. épocas', 'Tam. bache', 'MSE']
+    tcn_hypers = ['Nr. filtros', 'Tam. kernel', 'Dropout', 'Omitir conexiones', 'Usar Batch Normalization', 'Nr. épocas', 'Tam. bache', 'MSE']
+    rnn_hypers = ['Nr. capas', 'Nr. unidades LSTM', 'Dropout (lstm)', 'Nr nodos densos', 'Dropout (denso)','Nr. épocas', 'Tam. bache', 'MSE']
+    h = {
+        'cnn' : cnn_hypers,
+        'tcn': tcn_hypers,
+        'mlp': mlp_hypers,
+        'rnn': rnn_hypers
+    }
+   
+    users = [34,32]
+    pois = ['per', 'imp']
+
+    results = []
+    for poi in pois:
+        for u in users:
+            checkpoint_file = f'../../pkl/tunning/checkpoint_{arch}_{u}_{poi}.pkl'
+            res = load(checkpoint_file)
+            r = sorted(zip(res.func_vals, res.x_iters))[1]
+            best = [arch, u, poi, *r[1], r[0]]
+            results.append(best)
+
+    df_results = pd.DataFrame(data=results)
+    df_results.columns = ['arch', 'user', 'poi'] + h[arch]
+    float_df = df_results.select_dtypes(include='float')
+    float_cols = float_df.columns
+    for col in h[arch]:
+        if col in df_results.columns:
+            df_results[col] = 2 ** df_results[col]
+    df_results[float_cols] = np.round(float_df,3)
+    df_results.style.hide_index()
+    return  df_results
